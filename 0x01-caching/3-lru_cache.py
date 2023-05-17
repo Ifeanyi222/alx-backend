e19d1fb3151dea7e0d4c54e5d13d5bb17a2687f9
Ifeanyi222 <linusify@ymail.com>
2023-05-17 11:29:01 +0000

    Done

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Create a class LRUCache that inherits from BaseCaching and is a caching system:
+You must use self.cache_data - dictionary from the parent class BaseCaching
+You can overload def __init__(self): but don’t forget to call the parent init: 
+super().__init__()
+def put(self, key, item):
+Must assign to the dictionary self.cache_data the item value for the key key.
+If key or item is None, this method should not do anything.
+If the number of items in self.cache_data is higher that BaseCaching.MAX_ITEMS:
+"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LRUCache(BaseCaching):
+
+    def __init__(self):
+        
+        super().__init__()
+        self.usedKeys = []
+
+    def put(self, key, item):
+        
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+            if key not in self.usedKeys:
+                self.usedKeys.append(key)
+            else:
+                self.usedKeys.append(
+                    self.usedKeys.pop(self.usedKeys.index(key)))
+            if len(self.usedKeys) > BaseCaching.MAX_ITEMS:
+                discard = self.usedKeys.pop(0)
+                del self.cache_data[discard]
+                print('DISCARD: {:s}'.format(discard))
+
+    def get(self, key):
+    
+        if key is not None and key in self.cache_data.keys():
+            self.usedKeys.append(self.usedKeys.pop(self.usedKeys.index(key)))
+            return self.cache_data.get(key)
+        return None
+
